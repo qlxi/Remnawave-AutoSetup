ade5c76a0255b044a734af66dc9f9ec56eab9b61
AsanFillter <161594161+AsanFillter@users.noreply.github.com>
2025-02-26 19:54:01 +0330

    start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Downloading and preparing Remnawave installer..."
+wget -O install_temp.sh https://raw.githubusercontent.com/AsanFillter/Remnawave-AutoSetup/main/install_remnawave.sh
+sed -i 's/\r$//' install_temp.sh
+mv install_temp.sh install_remnawave.sh
+chmod +x install_remnawave.sh
+./install_remnawave.sh
